13253fd0a4e0143f35a17d8ebd7173543b7498c2
Zoey Hamm <339622581@GoTVDSB.ca>
2025-03-31 14:01:13 -0400

    Drew to screen and added Enumeration...

--- Game1.cs
+++ Game1.cs
@@ -6,6 +6,12 @@ using System;
 
 namespace Monogame_2___Lists___Loops
 {
+    enum ScreenState
+    {
+        TitleScreen,
+        MainScreen
+    }
+
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
@@ -16,6 +22,10 @@ namespace Monogame_2___Lists___Loops
         Texture2D spaceBackgroundTexture;
         List<Texture2D> textures;
         List<Rectangle> planetRects;
+        List<Texture2D> planetTextures;
+        ScreenState screenState;
+        MouseState mouseState;
+        KeyboardState keyboardState;
 
         public Game1()
         {
@@ -26,14 +36,16 @@ namespace Monogame_2___Lists___Loops
 
         protected override void Initialize()
         {
+            screenState = ScreenState.TitleScreen;
             window = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth,  _graphics.PreferredBackBufferHeight);
             generator = new Random();
+            planetTextures = new List<Texture2D>();
             textures = new List<Texture2D>();
             planetRects = new List<Rectangle>();
 
-            for (int i = 0; i < 30; i++)
+            for (int i = 0; i < generator.Next(10, 51); i++)
             {
-                planetRects.Add(new Rectangle(generator.Next(window.Width - 25), generator.Next(window.Height - 25), 25, 25)); // Challenge: Make rectangle have random sizes...
+                planetRects.Add(new Rectangle(generator.Next(window.Width - 25), generator.Next(window.Height - 25), 50, 50)); 
             }
 
             base.Initialize();
@@ -43,24 +55,51 @@ namespace Monogame_2___Lists___Loops
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            spaceBackgroundTexture = Content.Load<Texture2D>("Images/space_background");
+
+
+            for (int i = 1; i <= 13; i++)
+                textures.Add(Content.Load<Texture2D>("Images/16-bit-planet" + i));
+
+            for (int i = 0; i < planetRects.Count; i++)
+                planetTextures.Add(textures[generator.Next(textures.Count)]);
+
         }
 
         protected override void Update(GameTime gameTime)
         {
+            keyboardState = Keyboard.GetState();
+            mouseState = Mouse.GetState();
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            if (screenState == ScreenState.TitleScreen)
+            {
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                {
+                    screenState = ScreenState.MainScreen;
+                }
+            }
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            if (screenState == ScreenState.TitleScreen)
+            {
+                GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            }
+            else if (screenState == ScreenState.MainScreen)
+            {
+                _spriteBatch.Draw(spaceBackgroundTexture, window, Color.White);
+                for (int i = 0; i < planetRects.Count; i++)
+                    _spriteBatch.Draw(planetTextures[i], planetRects[i], Color.White);
+            }
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
