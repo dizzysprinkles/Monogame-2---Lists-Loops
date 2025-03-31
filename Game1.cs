30d77150705e038bb10f93f19c7e52fb98838ae3
Zoey Hamm <339622581@GoTVDSB.ca>
2025-03-31 14:33:50 -0400

    Finished Tutorial

--- Game1.cs
+++ Game1.cs
@@ -3,14 +3,15 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
 using System;
+using System.Reflection;
 
 namespace Monogame_2___Lists___Loops
 {
-    enum ScreenState
-    {
-        TitleScreen,
-        MainScreen
-    }
+    //enum ScreenState
+    //{
+    //    TitleScreen,
+    //    MainScreen
+    //}
 
     public class Game1 : Game
     {
@@ -23,9 +24,10 @@ namespace Monogame_2___Lists___Loops
         List<Texture2D> textures;
         List<Rectangle> planetRects;
         List<Texture2D> planetTextures;
-        ScreenState screenState;
-        MouseState mouseState;
+        //ScreenState screenState;
+        MouseState mouseState, previousMouseState;
         KeyboardState keyboardState;
+        float seconds, respawnTime;
 
         public Game1()
         {
@@ -36,17 +38,20 @@ namespace Monogame_2___Lists___Loops
 
         protected override void Initialize()
         {
-            screenState = ScreenState.TitleScreen;
+           // screenState = ScreenState.TitleScreen;
             window = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth,  _graphics.PreferredBackBufferHeight);
             generator = new Random();
             planetTextures = new List<Texture2D>();
             textures = new List<Texture2D>();
             planetRects = new List<Rectangle>();
 
-            for (int i = 0; i < generator.Next(10, 51); i++)
+            for (int i = 0; i < 1; i++)
             {
                 planetRects.Add(new Rectangle(generator.Next(window.Width - 25), generator.Next(window.Height - 25), 50, 50)); 
             }
+
+            seconds = 0f;
+            respawnTime = 5f;// #f = seconds. so 3f = 3 seconds
 
             base.Initialize();
         }
@@ -70,17 +75,48 @@ namespace Monogame_2___Lists___Loops
         {
             keyboardState = Keyboard.GetState();
             mouseState = Mouse.GetState();
+            seconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            if (screenState == ScreenState.TitleScreen)
+            if (mouseState.ScrollWheelValue > respawnTime || mouseState.ScrollWheelValue < respawnTime)
             {
-                if (mouseState.LeftButton == ButtonState.Pressed)
+                respawnTime = mouseState.ScrollWheelValue;
+            }
+            if (seconds > respawnTime)
+            {
+                planetTextures.Add(textures[generator.Next(textures.Count)]);
+                planetRects.Add(new Rectangle(generator.Next(window.Width - 25), generator.Next(window.Height - 25), 50, 50));
+                seconds = 0f; // Restarts timer
+            }
+
+            if (mouseState.LeftButton == ButtonState.Pressed && previousMouseState.LeftButton == ButtonState.Released)
+            {
+                for (int i = 0; i < planetRects.Count; i++)
                 {
-                    screenState = ScreenState.MainScreen;
+                    if (planetRects[i].Contains(mouseState.Position))
+                    {
+                        planetRects.RemoveAt(i);
+                        planetTextures.RemoveAt(i);
+                        i--;
+                    }
                 }
             }
 
+            if (planetTextures.Count == 0)
+            {
+                Exit();
+            }
+
+            //if (screenState == ScreenState.TitleScreen)
+            //{
+            //    if (mouseState.LeftButton == ButtonState.Pressed)
+            //    {
+            //        screenState = ScreenState.MainScreen;
+            //    }
+            //}
+
+            previousMouseState = mouseState;
             base.Update(gameTime);
         }
 
@@ -88,17 +124,18 @@ namespace Monogame_2___Lists___Loops
         {
 
             _spriteBatch.Begin();
-            if (screenState == ScreenState.TitleScreen)
-            {
-                GraphicsDevice.Clear(Color.CornflowerBlue);
+            //if (screenState == ScreenState.TitleScreen)
+            //{
+            //    GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            }
-            else if (screenState == ScreenState.MainScreen)
+            //}
+
+            _spriteBatch.Draw(spaceBackgroundTexture, window, Color.White);
+            for (int i = 0; i < planetRects.Count; i++)
             {
-                _spriteBatch.Draw(spaceBackgroundTexture, window, Color.White);
-                for (int i = 0; i < planetRects.Count; i++)
-                    _spriteBatch.Draw(planetTextures[i], planetRects[i], Color.White);
+               _spriteBatch.Draw(planetTextures[i], planetRects[i], Color.White);
             }
+    
             _spriteBatch.End();
 
             base.Draw(gameTime);
